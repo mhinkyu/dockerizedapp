397ccd93448993a0e2c7a868fe548a494643d8bf
Mhinkyu <mhinkyuk@gmail.com>
2022-10-19 00:37:53 +0300

    new file added to gitignore

--- user_data.sh
+++ user_data.sh
@@ -4,7 +4,7 @@ sudo yum update -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-# API_KEY=$(aws ssm get-paramter --query Parameter.Value --name api_key --output text)
+# API_KEY=$(aws ssm get-paramter --query Parameter.Value --name API_key --output text)
 sudo amazon-linux-extras install docker -y
 sudo systemctl start docker
 sudo usermod -a -G docker ec2-user
@@ -15,7 +15,10 @@ sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo yum install git -y
 git clone https://github.com/mhinkyu/dockerizedapp.git
 sudo cd ./MoviePoster-web/
-sudo docker build . -t app:latest
-sudo docker-compose -f Movie_compose.yaml up
+docker build . -t app:latest
+docker exec -it dockerizedapp-app-1 sh
+export API_KEY=53368837602b81de55a03445d3929369
+exit 
+docker-compose -f Movie_compose.yaml up
 
 #make sure that the ec2 user has IAM ROLE to access the SSM parameter store.
